195fc80dfd57a54d7d9ab3c2c1a9f4014d357a65
Dorian Black <d.black.personal@gmail.com>
2023-11-09 18:31:03 +0200

    v1.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,26 @@ import { StyledTitle } from './Filter/Filter.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    try {
+      const storageContacts = JSON.parse(localStorage.getItem('contacts'));
+      if (storageContacts) {
+        this.setState({ contacts: storageContacts });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = newContact => {
     if (
@@ -40,11 +52,15 @@ export class App extends Component {
     }));
   };
 
-  render() {
+  filterContacts() {
     const { contacts, filter } = this.state;
-    const filteredContacts = contacts.filter(contact =>
+    return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
+  }
+
+  render() {
+    const filteredContacts = this.filterContacts();
 
     return (
       <div>
